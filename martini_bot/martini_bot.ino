ee649204834c7ed27a0d2829c067e98602965268
Peter Wilson <pw12nb@brocku.ca>
2016-04-13 16:16:01 -0400

    added some code

--- martini_bot/martini_bot.ino
+++ martini_bot/martini_bot.ino
@@ -1,19 +1,36 @@
+bool findingMode;
+int distanceToMove;
+
 void setup() {  
   // setup the xbee connection 
   Serial.begin(115200);
+  findingMode = false;
+  distanceToMove = 0;
 }
 
 void loop() {
     
   //wait for user to enter the button
-  if(Serial.available > 0)
+  if(!findingMode && Serial.available > 0)
   {
-  //controller sends radio signal to start search
-  
-  //trigger for reading
-  //if no reading found, rotate 45 degrees trigger for reading (repeat)
+      //controller sends radio signal to start search
+      char input = Serial.read();
+      if(input == 'y')
+        findingMode = true;
+  }
 
-   //move forward x units, trigger for new reading (repeat)
-   
-   //when within threashold, stop
+  if(findingMode)
+  {
+    if(distanceToMove == 0)
+    {
+      //trigger for reading
+      Serial.write('s');
+    }
+    
+    //if no reading found, rotate 45 degrees trigger for reading (repeat)
+  
+     //move forward x units, trigger for new reading (repeat)
+     
+     //when within threashold, stop
+  }
 }
