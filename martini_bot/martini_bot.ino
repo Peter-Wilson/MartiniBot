02dc94c35f458a534e6d1214bcd1842ebd936c53
Peter Wilson <pw12nb@brocku.ca>
2016-04-13 16:07:27 -0400

    Added the caller code
    This will be for the trigger remote

--- martini_bot/martini_bot.ino
+++ martini_bot/martini_bot.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup() {  
+  // setup the xbee connection 
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    
+  //wait for user to enter the button
+  if(Serial.available > 0)
+  {
+  //controller sends radio signal to start search
+  
+  //trigger for reading
+  //if no reading found, rotate 45 degrees trigger for reading (repeat)
 
+   //move forward x units, trigger for new reading (repeat)
+   
+   //when within threashold, stop
 }
