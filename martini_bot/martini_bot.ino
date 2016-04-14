d46036876b918a878e05bf14c97e223e0c01a7d9
unknown <pw12nb@cosc-j32706.msad.cosc.brocku.ca>
2016-04-14 15:33:25 -0400

    added motor controls

--- martini_bot/martini_bot.ino
+++ martini_bot/martini_bot.ino
@@ -4,6 +4,10 @@ int distanceToMove;
 void setup() {  
   // setup the xbee connection 
   Serial.begin(115200);
+  
+  for(i=4;i<=7;i++)
+    pinMode(i, OUTPUT);  
+  
   findingMode = false;
   distanceToMove = 0;
 }
@@ -33,4 +37,45 @@ void loop() {
      
      //when within threashold, stop
   }
+}
+
+//Standard PWM DC control
+int E1 = 5;     //M1 Speed Control
+int E2 = 6;     //M2 Speed Control
+int M1 = 4;    //M1 Direction Control
+int M2 = 7;    //M1 Direction Control
+
+
+void stop(void)                    //Stop
+{
+  digitalWrite(E1,LOW);   
+  digitalWrite(E2,LOW);      
+}   
+void advance(char a,char b)          //Move forward
+{
+  analogWrite (E1,a);      //PWM Speed Control
+  digitalWrite(M1,HIGH);    
+  analogWrite (E2,b);    
+  digitalWrite(M2,HIGH);
+}  
+void back_off (char a,char b)          //Move backward
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,LOW);   
+  analogWrite (E2,b);    
+  digitalWrite(M2,LOW);
+}
+void turn_L (char a,char b)             //Turn Left
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,LOW);    
+  analogWrite (E2,b);    
+  digitalWrite(M2,HIGH);
+}
+void turn_R (char a,char b)             //Turn Right
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,HIGH);    
+  analogWrite (E2,b);    
+  digitalWrite(M2,LOW);
 }
